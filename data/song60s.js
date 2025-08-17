b670953e16a55df547eee474244afd98cdeecc2c
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:28:04 +0200

    Create song60s.js

---
+++ data/song60s.js
@@ -0,0 +1,5 @@
+const songs60s = [
+  { title: "Good Vibrations", artist: "The Beach Boys", src: "songs/60s/goodvibrations_beachboys.mp3" },
+  { title: "Hey Jude", artist: "The Beatles", src: "songs/60s/heyjude_thebeatles.mp3" },
+  { title: "Paint It Black", artist: "The Rolling Stones", src: "songs/60s/Paintitblack_rollingstones.mp3" }
+];

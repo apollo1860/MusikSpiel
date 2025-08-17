4a1284387b3672ab38d19fd3f98f980d14dcff4b
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:42:31 +0200

    Create songsByDecade.js

---
+++ songsByDecade.js
@@ -0,0 +1,10 @@
+const songsByDecade = [
+  { key: '50s', label: '1950er', songs: songs50s },
+  { key: '60s', label: '1960er', songs: songs60s },
+  { key: '70s', label: '1970er', songs: songs70s },
+  { key: '80s', label: '1980er', songs: songs80s },
+  { key: '90s', label: '1990er', songs: songs90s },
+  { key: '00s', label: '2000er', songs: songs2000s },   // oder '2000s' – aber dann überall gleich!
+  { key: '10s', label: '2010er', songs: songs2010s },
+  { key: '20s', label: '2020er', songs: songs2020s }
+];

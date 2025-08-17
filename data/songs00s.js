318e7987cf1dec907ac7ab74f33a544e3f81a25f
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:36:56 +0200

    Create songs00s.js

---
+++ data/songs00s.js
@@ -0,0 +1,5 @@
+const songs00s = [
+  { title: "Hey Ya!", artist: "Outkast", src: "songs/00s/heyya_outkast.mp3" },
+  { title: "In The End", artist: "Linkin Park", src: "songs/00s/intheend_linkinpark.mp3" },
+  { title: "Lose Yourself", artist: "Eminem", src: "songs/00s/loseyourself_eminem.mp3" }
+];

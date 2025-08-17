f8a71d9f88683f2f5bc24e5be2e27c7344b20333
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:41:21 +0200

    Create songs20s.js

---
+++ data/songs20s.js
@@ -0,0 +1,5 @@
+const songs20s = [
+  { title: "As It Was", artist: "Harry Styles", src: "songs/20s/asitwas_harrystyles.mp3" },
+  { title: "Blinding Lights", artist: "The Weeknd", src: "songs/20s/Blindinglights_theweeknd.mp3" },
+  { title: "Drivers License", artist: "Olivia Rodrigo", src: "songs/20s/driverslicense_oliviarodrigo.mp3" }
+];

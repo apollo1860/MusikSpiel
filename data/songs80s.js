ad1c5f2dcd20edfea58bab81e6fdd6b2dc8226d6
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:32:51 +0200

    Create songs80s.js

---
+++ data/songs80s.js
@@ -0,0 +1,5 @@
+const songs80s = [
+  { title: "Billie Jean", artist: "Michael Jackson", src: "songs/80s/billiejean_michaeljackson.mp3" },
+  { title: "Livin On A Prayer", artist: "Jon Bon Jovi", src: "songs/80s/livinonaprayer_bonjovi.mp3" },
+  { title: "Take On Me", artist: "a-ha", src: "songs/80s/takeonme_aha.mp3" }
+];

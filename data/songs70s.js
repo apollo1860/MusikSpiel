0cc9cea7322077f17ef96d1c525defe4d15aa4bb
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-17 19:30:33 +0200

    Create songs70s.js

---
+++ data/songs70s.js
@@ -0,0 +1,5 @@
+const songs70s = [
+  { title: "Bohemian Rhapsody", artist: "Queen", src: "songs/70s/bohemianrhapsody_queen.mp3" },
+  { title: "Dancing Queen", artist: "ABBA", src: "songs/70s/dancingqueen_abba.mp3" },
+  { title: "Stayin Alive", artist: "Bee Gees", src: "songs/70s/stayinalive_beegees.mp3" }
+];

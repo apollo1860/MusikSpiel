5afa242b5b0aa6462fbf03fea4cb52e9c6122673
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 12:44:21 +0200

    Update songsByDecade.js

--- data/songsByDecade.js
+++ data/songsByDecade.js
@@ -4,7 +4,7 @@ const songsByDecade = [
   { key: '70s', label: '1970er', songs: songs70s },
   { key: '80s', label: '1980er', songs: songs80s },
   { key: '90s', label: '1990er', songs: songs90s },
-  { key: '00s', label: '2000er', songs: songs2000s },   // oder '2000s' – aber dann überall gleich!
-  { key: '10s', label: '2010er', songs: songs2010s },
-  { key: '20s', label: '2020er', songs: songs2020s }
+  { key: '00s', label: '2000er', songs: songs00s },   // oder '2000s' – aber dann überall gleich!
+  { key: '10s', label: '2010er', songs: songs10s },
+  { key: '20s', label: '2020er', songs: songs20s }
 ];

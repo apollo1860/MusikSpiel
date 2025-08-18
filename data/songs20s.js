87735d8cb0bec6311cb28ef9213c2cc7b941c3d5
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 10:19:16 +0200

    Update songs20s.js

--- data/songs20s.js
+++ data/songs20s.js
@@ -1,5 +1,5 @@
 const songs20s = [
-  { title: "As It Was", artist: "Harry Styles", src: "songs/20s/asitwas_harrystyles.mp3" },
-  { title: "Blinding Lights", artist: "The Weeknd", src: "songs/20s/Blindinglights_theweeknd.mp3" },
-  { title: "Drivers License", artist: "Olivia Rodrigo", src: "songs/20s/driverslicense_oliviarodrigo.mp3" }
+  { title: "As It Was", artist: "Harry Styles", year:2022, src: "songs/20s/asitwas_harrystyles.mp3" },
+  { title: "Blinding Lights", artist: "The Weeknd", year:2019, src: "songs/20s/Blindinglights_theweeknd.mp3" },
+  { title: "Drivers License", artist: "Olivia Rodrigo", year:2021, src: "songs/20s/driverslicense_oliviarodrigo.mp3" }
 ];

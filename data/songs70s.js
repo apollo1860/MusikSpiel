2ea9da316078913ea6f55c8f3d5093e5523ed766
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 10:23:06 +0200

    Update songs70s.js

--- data/songs70s.js
+++ data/songs70s.js
@@ -1,5 +1,5 @@
 const songs70s = [
-  { title: "Bohemian Rhapsody", artist: "Queen", src: "songs/70s/bohemianrhapsody_queen.mp3" },
-  { title: "Dancing Queen", artist: "ABBA", src: "songs/70s/dancingqueen_abba.mp3" },
-  { title: "Stayin Alive", artist: "Bee Gees", src: "songs/70s/stayinalive_beegees.mp3" }
+  { title: "Bohemian Rhapsody", artist: "Queen", year:1975, src: "songs/70s/bohemianrhapsody_queen.mp3" },
+  { title: "Dancing Queen", artist: "ABBA", year:1976, src: "songs/70s/dancingqueen_abba.mp3" },
+  { title: "Stayin Alive", artist: "Bee Gees", year:1977, src: "songs/70s/stayinalive_beegees.mp3" }
 ];

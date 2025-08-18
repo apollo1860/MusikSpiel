30baaff0ceb7a610d456dcc5d0b82274d4ad4679
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 10:21:06 +0200

    Update songs50s.js

--- data/songs50s.js
+++ data/songs50s.js
@@ -1,5 +1,5 @@
 const songs50s = [
-  { title: "Jailhouse Rock", artist: "Elvis Presley", src: "songs/50s/Jailhouserock_elvispresley.mp3" },
-  { title: "Johnny B. Goode", artist: "Chuck Berry", src: "songs/50s/johnnybgoode_chuckberry.mp3" },
-  { title: "Rock Around The Clock", artist: "Bill Haley", src: "songs/50s/rockaroundtheclock_billhaley.mp3" }
+  { title: "Jailhouse Rock", artist: "Elvis Presley", year:1957, src: "songs/50s/Jailhouserock_elvispresley.mp3" },
+  { title: "Johnny B. Goode", artist: "Chuck Berry", year:1958, src: "songs/50s/johnnybgoode_chuckberry.mp3" },
+  { title: "Rock Around The Clock", artist: "Bill Haley", year: 1954, src: "songs/50s/rockaroundtheclock_billhaley.mp3" }
 ];

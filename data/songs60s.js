9c9cea549d9ec66ddc00df3150e6e74984278a8f
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 10:22:04 +0200

    Update songs60s.js

--- data/songs60s.js
+++ data/songs60s.js
@@ -1,5 +1,5 @@
 const songs60s = [
-  { title: "Good Vibrations", artist: "The Beach Boys", src: "songs/60s/goodvibrations_beachboys.mp3" },
-  { title: "Hey Jude", artist: "The Beatles", src: "songs/60s/heyjude_thebeatles.mp3" },
-  { title: "Paint It Black", artist: "The Rolling Stones", src: "songs/60s/Paintitblack_rollingstones.mp3" }
+  { title: "Good Vibrations", artist: "The Beach Boys", year:1966, src: "songs/60s/goodvibrations_beachboys.mp3" },
+  { title: "Hey Jude", artist: "The Beatles", year:1968, src: "songs/60s/heyjude_thebeatles.mp3" },
+  { title: "Paint It Black", artist: "The Rolling Stones", year:1966, src: "songs/60s/Paintitblack_rollingstones.mp3" }
 ];

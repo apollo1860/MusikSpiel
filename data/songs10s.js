a83fcf9cf822eafd155affbedb7cb46cfb308bb2
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-18 10:14:58 +0200

    Update songs10s.js

--- data/songs10s.js
+++ data/songs10s.js
@@ -1,5 +1,5 @@
 const songs10s = [
-  { title: "Rolling In The Deep", artist: "Adele", src: "songs/10s/rollinginthedeep_adele.mp3" },
-  { title: "Shape Of You", artist: "Ed Sheeran", src: "songs/10s/shapeofyou_edsheeran.mp3" },
-  { title: "Uptown Funk", artist: "Bruno Mars, Mark Ronson", src: "songs/10s/uptownfunk_brunomars-markronson.mp3" }
+  { title: "Rolling In The Deep", artist: "Adele", year:2010, src: "songs/10s/rollinginthedeep_adele.mp3" },
+  { title: "Shape Of You", artist: "Ed Sheeran", year:2017, src: "songs/10s/shapeofyou_edsheeran.mp3" },
+  { title: "Uptown Funk", artist: "Bruno Mars, Mark Ronson", year:2014, src: "songs/10s/uptownfunk_brunomars-markronson.mp3" }
 ];

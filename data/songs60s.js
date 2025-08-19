253701bc6d38c88f39e646bd27afa755e6a35b8b
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:13:07 +0200

    Update songs60s.js

--- data/songs60s.js
+++ data/songs60s.js
@@ -3,3 +3,5 @@ const songs60s = [
   { title: "Hey Jude", artist: "The Beatles", year:1968, src: "songs/60s/heyjude_thebeatles.mp3" },
   { title: "Paint It Black", artist: "The Rolling Stones", year:1966, src: "songs/60s/Paintitblack_rollingstones.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'60s', label:'1960er', list: songs60s });

0e87b8636ac3034690e5cb4d54211000e4c14eee
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:15:08 +0200

    Update songs20s.js

--- data/songs20s.js
+++ data/songs20s.js
@@ -3,3 +3,5 @@ const songs20s = [
   { title: "Blinding Lights", artist: "The Weeknd", year:2019, src: "songs/20s/Blindinglights_theweeknd.mp3" },
   { title: "Drivers License", artist: "Olivia Rodrigo", year:2021, src: "songs/20s/driverslicense_oliviarodrigo.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'20s', label:'2020er', list: songs20s });

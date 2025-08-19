1b1db782be2e6778d25aad707c57ab5b2425b1de
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:12:47 +0200

    Update songs50s.js

--- data/songs50s.js
+++ data/songs50s.js
@@ -3,3 +3,6 @@ const songs50s = [
   { title: "Johnny B. Goode", artist: "Chuck Berry", year:1958, src: "songs/50s/johnnybgoode_chuckberry.mp3" },
   { title: "Rock Around The Clock", artist: "Bill Haley", year: 1954, src: "songs/50s/rockaroundtheclock_billhaley.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'50s', label:'1950er', list: songs50s });
+

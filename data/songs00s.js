edb2329c6aa4bdd9d5aefb856e56d8eac7a7b0d2
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:14:29 +0200

    Update songs00s.js

--- data/songs00s.js
+++ data/songs00s.js
@@ -3,3 +3,5 @@ const songs00s = [
   { title: "In The End", artist: "Linkin Park", year: 2001, src: "songs/00s/intheend_linkinpark.mp3" },
   { title: "Lose Yourself", artist: "Eminem", year: 2002, src: "songs/00s/loseyourself_eminem.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'00s', label:'2000er', list: songs00s });

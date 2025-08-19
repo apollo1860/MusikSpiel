dee18b2a6f16d0c90e63f1314b1671bbb293af8e
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:14:50 +0200

    Update songs10s.js

--- data/songs10s.js
+++ data/songs10s.js
@@ -3,3 +3,5 @@ const songs10s = [
   { title: "Shape Of You", artist: "Ed Sheeran", year:2017, src: "songs/10s/shapeofyou_edsheeran.mp3" },
   { title: "Uptown Funk", artist: "Bruno Mars, Mark Ronson", year:2014, src: "songs/10s/uptownfunk_brunomars-markronson.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'10s', label:'2010er', list: songs10s });

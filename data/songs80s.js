f4751b1e00af2ff7a3090319038610624ba9cfc0
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-19 16:13:39 +0200

    Update songs80s.js

--- data/songs80s.js
+++ data/songs80s.js
@@ -3,3 +3,5 @@ const songs80s = [
   { title: "Livin On A Prayer", artist: "Jon Bon Jovi", year: 1986, src: "songs/80s/livinonaprayer_bonjovi.mp3" },
   { title: "Take On Me", artist: "a-ha", year:1984, src: "songs/80s/takeonme_aha.mp3" }
 ];
+window.SONG_DECADES = window.SONG_DECADES || [];
+window.SONG_DECADES.push({ key:'80s', label:'1980er', list: songs80s });

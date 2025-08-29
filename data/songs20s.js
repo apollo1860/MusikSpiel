9824b2398be1b7e2be07139fff1abbc0e5f18925
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-29 18:46:27 +0200

    Update songs20s.js

--- data/songs20s.js
+++ data/songs20s.js
@@ -2,6 +2,17 @@
 window.BASE_URL ??= "https://pub-3910a02fcd5042f5bb1910363e89357c.r2.dev/";
 
 const songs20s = [
+  { title: "Bad Habits", artist: "Ed Sheeran", year: 2021, src: window.BASE_URL + "20s/badhabits_edsheeran.mp3" },
+  { title: "Shivers", artist: "Ed Sheeran", year: 2021, src: window.BASE_URL + "20s/shivers_edsheeran.mp3" },
+  { title: "Eyes Closed", artist: "Ed Sheeran", year: 2023, src: window.BASE_URL + "20s/eyesclosed_edsheeran.mp3" },
+  { title: "Peaches", artist: "Justin Bieber feat. Daniel Caesar & Giveon", year: 2021, src: window.BASE_URL + "20s/peaches_justinbieber.mp3" },
+  { title: "Stay", artist: "The Kid Laroi & Justin Bieber", year: 2021, src: window.BASE_URL + "20s/stay_thekidlaroi.mp3" },
+  { title: "Ghost", artist: "Justin Bieber", year: 2021, src: window.BASE_URL + "20s/ghost_justinbieber.mp3" },
+  { title: "About Damn Time", artist: "Lizzo", year: 2022, src: window.BASE_URL + "20s/aboutdamntime_lizzo.mp3" },
+  { title: "Rumors", artist: "Lizzo feat. Cardi B", year: 2021, src: window.BASE_URL + "20s/rumors_lizzo.mp3" },
+  { title: "Espresso", artist: "Sabrina Carpenter", year: 2024, src: window.BASE_URL + "20s/espresso_sabrinacarpenter.mp3" },
+  { title: "Bagchaser Can", artist: "Pashanim", year: 2023, src: window.BASE_URL + "20s/bagchasercan_pashanim.mp3" },
+  { title: "Blaues Licht", artist: "RAF Camora", year: 2023, src: window.BASE_URL + "20s/blaueslicht_rafcamora.mp3" },
   { title: "As It Was", artist: "Harry Styles", year: 2022, src: window.BASE_URL + "20s/asitwas_harrystyles.mp3" },
   { title: "Blinding Lights", artist: "The Weeknd", year: 2019, src: window.BASE_URL + "20s/Blindinglights_theweeknd.mp3" },
   { title: "Drivers License", artist: "Olivia Rodrigo", year: 2021, src: window.BASE_URL + "20s/driverslicense_oliviarodrigo.mp3" },

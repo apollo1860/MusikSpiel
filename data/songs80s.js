06c65f91c6eb0b5e46d4b2196aa24d87c0a5931f
apollo1860 <167482632+apollo1860@users.noreply.github.com>
2025-08-29 15:37:45 +0200

    Update songs80s.js

--- data/songs80s.js
+++ data/songs80s.js
@@ -1,6 +1,9 @@
 const BASE_URL = "https://pub-3910a02fcd5042f5bb1910363e89357c.r2.dev/";
 
 const songs80s = [
+  { title: "Billie Jean", artist: "Michael Jackson", year:1982, src: BASE_URL + "80s/billiejean_michaeljackson.mp3" },
+  { title: "Livin On A Prayer", artist: "Jon Bon Jovi", year: 1986, src: BASE_URL + "80s/livinonaprayer_bonjovi.mp3" },
+  { title: "Take On Me", artist: "a-ha", year:1984, src: BASE_URL + "80s/takeonme_aha.mp3" },
   { title: "Beat It", artist: "Michael Jackson", year: 1982, src: BASE_URL + "80s/beatit_michaeljackson.mp3" },
   { title: "Thriller", artist: "Michael Jackson", year: 1982, src: BASE_URL + "80s/thriller_michaeljackson.mp3" },
   { title: "Like a Virgin", artist: "Madonna", year: 1984, src: BASE_URL + "80s/likeavirgin_madonna.mp3" },
